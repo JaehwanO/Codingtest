4e2bf884c3d861708109207815068b436562b99a
JaehwanO <tkswjr235@naver.com>
2022-08-15 21:14:19 +0900

    2022/08/15

---
+++ greedy/#2212.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+sensor = list(map(int,input().split()))
+sensor.sort()
+
+array = []
+for i in range(0,n-1):
+    array.append(sensor[i+1] - sensor[i])
+
+array.sort()
+
+print(sum(array[:n-k]))
